6ce5156b570c308eee7a5933e79acd220d3048bd
Abdelhalim-lhm <Abdelhalim.lacham@gmail.com>
2024-02-27 12:07:46 -0800

    3. Delete

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - function that inserts a node
+ * binary_tree_insert_right - function that inserts a node
  * as the right-child of another node
  * @parent: pointer to the parent node of the node to create
  * @value: the value to put in the new node
